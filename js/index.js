c62d85b762ab29b1513b54c65fd1c1f68f32d709
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-06-05 13:09:53 +0200

    add game

--- js/index.js
+++ js/index.js
@@ -1,35 +1,91 @@
-'use strict';
-(function(){ 
-	var showModal = function(event){	
-    event.preventDefault();	
-    document.querySelector('#modal-overlay').classList.add('show');
-     document.querySelector('#modal-one').classList.add('show');
-	};
-	var modalLinks = document.querySelectorAll('.show-modal');
-	
-	for(var i = 0; i < modalLinks.length; i++){
-		modalLinks[i].addEventListener('click', showModal);
-	}
-	
-	var hideModal = function(event){
-		event.preventDefault();
-		document.querySelector('#modal-overlay').classList.remove('show');
-	};
-	
-	var closeButtons = document.querySelectorAll('.modal .close');
-	
-	for(var i = 0; i < closeButtons.length; i++){
-		closeButtons[i].addEventListener('click', hideModal);
-	}
+   
 
-	document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+var box1 = document.getElementById('box1');
+var ramkaphoto = document.getElementById('output1');
+box1.addEventListener('click', function(){
+  var kompChoice = Math.random();
+        if (kompChoice < 0.34) {
+            kompChoice = "kamien";
+        } else if(kompChoice <= 0.67) {
+            kompChoice = "papier";
+        } else {
+            kompChoice = "nozyce";
+        }
+var compare = function() {
+  if (kompChoice == "nozyce");{
+   var wynik = ' wygrana ' ;
+  }
+  if (kompChoice == "kamien"){
+    var wynik = ' remis '
+  }
+  if (kompChoice == "papier"){
+   var wynik = 'przegrana'
+  }
+  
+  return wynik;
+  
+ }
 
-	var modals = document.querySelectorAll('.modal');
-	
-	for(var i = 0; i < modals.length; i++){
-		modals[i].addEventListener('click', function(event){
-			event.stopPropagation();
-		});
-	}
 
-})();
+
+var output = document.getElementById('output1');
+output.innerHTML = ' Ty wybrałeś:KAMIEN a przeciwnik:'+kompChoice+' czyli...'+ compare() ;
+}        
+
+                      
+);
+var box2 = document.getElementById('box2'); 
+box2.addEventListener('click', function(){
+var kompChoice = Math.random();
+        if (kompChoice < 0.34) {
+            kompChoice = "kamien";
+        } else if(kompChoice <= 0.67) {
+            kompChoice = "papier";
+        } else {
+            kompChoice = "nozyce";
+        }
+  var porownaj=function(){
+    if(kompChoice==="nozyce"){
+      var wynik = " remis !"
+    }
+   if(kompChoice==="kamien"){
+     var wynik = " przegrana !"
+   }
+   if(kompChoice==="papier"){
+     var wynik =" wygrana! "
+   }
+    return wynik;
+  }
+
+var output = document.getElementById('output1');
+output.innerHTML = 'Ty wybrałeś:Nozyce a przeciwnik: '+kompChoice+porownaj();
+}                   
+);
+var box3 = document.getElementById('box3'); 
+box3.addEventListener('click', function(){
+var kompChoice = Math.random();
+        if (kompChoice < 0.34) {
+            kompChoice = "kamien";
+        } else if(kompChoice <= 0.67) {
+            kompChoice = "papier";
+        } else {
+            kompChoice = "nozyce";
+        }
+ 
+  var porownaj=function(){
+    if(kompChoice==="papier"){
+      var wynik = " remis !"
+    }
+   if(kompChoice==="nozyce"){
+     var wynik = " przegrana !"
+   }
+   if(kompChoice==="kamien"){
+     var wynik =" wygrana! "
+   }
+    return wynik;
+  }
+  
+var output = document.getElementById('output1');
+output.innerHTML = 'Ty wybrałeś:PAPIER a przeciwnik: '+kompChoice+porownaj();
+}                   
+);
